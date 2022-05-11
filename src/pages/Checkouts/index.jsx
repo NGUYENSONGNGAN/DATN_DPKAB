4924a467466d22ada90ce98236e59fbfdb6398bc
ngan <ngan@s3corp.com.vn>
2022-05-11 17:39:47 +0700

    DA:update checkout

--- src/pages/Checkouts/index.jsx
+++ src/pages/Checkouts/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './styles.scss'
 import Box from '@mui/material/Box';
@@ -7,30 +7,44 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import axios from "axios"
 
 
 
 function CheckOuts() {
 
+    const [age1, setAge1] = React.useState('');
+    const [age2, setAge2] = React.useState('');
+    const [age3, setAge3] = React.useState('');
+    const [names, setNames] = React.useState([]);
+    const [districts, setDistricts] = React.useState({});
 
-    const names = [
-        'Oliver Hansen',
-        'Van Henry',
-        'April Tucker',
-        'Ralph Hubbard',
-        'Omar Alexander',
-        'Carlos Abbott',
-        'Miriam Wagner',
-        'Bradley Wilkerson',
-        'Virginia Andrews',
-        'Kelly Snyder',
-    ];
+    useEffect(() => {
+        const fetchNames = async () => {
+            axios.get("https://provinces.open-api.vn/api/?depth=3")
+                .then((response) => setNames(response.data));
+        }
+        fetchNames();
+    }, [])
 
-    const [age, setAge] = React.useState('');
+    const [listH, setlistH] = useState([]);
+    const [listX, setlistX] = useState([]);
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
+    const handleChangeT = (event) => {
+        setAge1(event.target.value);
+        const itemFilter = names.find((item) => item.code == event.target.value);
+        setlistH(itemFilter.districts)
     };
+    const handleChange = (event) => {
+        setAge2(event.target.value);
+        const itemxa = listH.find((item) => item.code == event.target.value);
+        console.log('itemxa', itemxa)
+        setlistX(itemxa.wards)
+        console.log('itemxa.wards', itemxa.wards)
+    }
+    const handleChangeP = (event) => {
+        setAge3(event.target.value);
+    }
 
 
 
@@ -92,58 +106,59 @@ function CheckOuts() {
                                 <div className='row'>
                                     <div className='box-left col-md-4'>
                                         <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                                            <InputLabel id="demo-select-small">Age</InputLabel>
+                                            <InputLabel id="demo-select-small">Tỉnh/Thành phố</InputLabel>
                                             <Select
                                                 labelId="demo-select-small"
                                                 id="demo-select-small"
-                                                value={age}
+                                                value={age1}
                                                 label="Age"
-                                                onChange={handleChange}
+                                                onChange={handleChangeT}
                                             >
-                                                <MenuItem value="">
-                                                    <em>None</em>
-                                                </MenuItem>
                                                 {names.map((item) => {
-                                                    return <MenuItem value={10} key={item}>{item}</MenuItem>
+                                                    return <MenuItem value={item.code} key={item.code}>{item.name}</MenuItem>
+
                                                 })}
                                             </Select>
                                         </FormControl>
                                     </div>
                                     <div className='box-left col-md-4'>
                                         <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                                            <InputLabel id="demo-select-small">Age</InputLabel>
+                                            <InputLabel id="demo-select-small">Quận huyện</InputLabel>
                                             <Select
                                                 labelId="demo-select-small"
                                                 id="demo-select-small"
-                                                value={age}
+                                                value={age2}
                                                 label="Age"
                                                 onChange={handleChange}
                                             >
-                                                <MenuItem value="">
-                                                    <em>None</em>
-                                                </MenuItem>
-                                                {names.map((item) => {
-                                                    return <MenuItem value={10} key={item}>{item}</MenuItem>
+
+                                                {listH.length && listH.map((item) => {
+                                                    return <MenuItem value={item.code} key={item.code}>{item.name}</MenuItem>
+
                                                 })}
+
+
+
+                                                {/* <MenuItem value={10} key={districts.code}>{districts.name}</MenuItem> */}
                                             </Select>
                                         </FormControl>
                                     </div>
                                     <div className='box-left col-md-4'>
                                         <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                                            <InputLabel id="demo-select-small">Age</InputLabel>
+                                            <InputLabel id="demo-select-small">Xã/phường</InputLabel>
                                             <Select
                                                 labelId="demo-select-small"
                                                 id="demo-select-small"
-                                                value={age}
+                                                value={age3}
                                                 label="Age"
-                                                onChange={handleChange}
+                                                onChange={handleChangeP}
                                             >
-                                                <MenuItem value="">
-                                                    <em>None</em>
-                                                </MenuItem>
-                                                {names.map((item) => {
-                                                    return <MenuItem value={10} key={item}>{item}</MenuItem>
+                                                {listX.length && listX.map((item) => {
+                                                    return <MenuItem value={item.code} key={item.code}>{item.name}</MenuItem>
+
                                                 })}
+
+
                                             </Select>
                                         </FormControl>
                                     </div>
@@ -152,8 +167,70 @@ function CheckOuts() {
                             </div>
                         </div>
 
+                        <div className="section-header">
+                            <h2 className="section-title ">Phương thức thanh toán</h2>
+                        </div>
+                        <div className="content-box">
 
+                            <div className="radio-wrapper content-box-row">
+                                <label className="radio-label" htmlFor="payment_method_id_1002364966">
+                                    <div className="radio-input payment-method-checkbox">
+                                        <input type-id="4" id="payment_method_id_1002364966" className="input-radio" name="payment_method_id" type="radio" value="1002364966" />
+                                    </div>
+                                    <div className="radio-content-input">
+                                        <img className="main-img" src="https://hstatic.net/0/0/global/design/seller/image/payment/other.svg?v=1" />
+                                        <div>
+                                            <span className="radio-label-primary">Chuyển khoản qua ngân hàng</span>
+                                            <span className="quick-tagline hidden"></span>
+                                            <span className="quick-tagline  hidden ">Buy Now, Pay Later</span>
+                                        </div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="radio-wrapper content-box-row">
+                                <label className="radio-label" for="payment_method_id_1002364968">
+                                    <div className="radio-input payment-method-checkbox">
+                                        <input type-id="1" id="payment_method_id_1002364968" className="input-radio" name="payment_method_id" type="radio" value="1002364968" />
+                                    </div>
+                                    <div className="radio-content-input">
+                                        <img className="main-img" src="https://hstatic.net/0/0/global/design/seller/image/payment/cod.svg?v=1" />
+                                        <div>
+                                            <span className="radio-label-primary">Thanh toán khi giao hàng (COD)</span>
+                                            <span className="quick-tagline hidden"></span>
+                                            <span className="quick-tagline  hidden ">Buy Now, Pay Later
 
+                                            </span></div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="radio-wrapper content-box-row">
+                                <label className="radio-label" for="payment_method_id_1002373494">
+                                    <div className="radio-input payment-method-checkbox">
+                                        <input type-id="8" id="payment_method_id_1002373494" className="input-radio" name="payment_method_id" type="radio" value="1002373494" />
+                                    </div>
+                                    <div className="radio-content-input">
+                                        <img className="main-img" src="https://hstatic.net/0/0/global/design/seller/image/payment/vnpay_new.svg?v=1" />
+                                        <div>
+                                            <span className="radio-label-primary">Thẻ ATM/Visa/Master/JCB/QR Pay qua cổng VNPAY</span>
+                                            <span className="quick-tagline hidden"></span>
+                                            <span className="quick-tagline  hidden ">Buy Now, Pay Later
+                                            </span>
+                                        </div>
+                                    </div>
+                                </label>
+                            </div>
+
+                            <div class="step-footer">
+                                <Link class="step-footer-previous-link" to="/cart">
+                                    Giỏ hàng
+                                </Link>
+                                <Link to='/payment'>
+                                <button type="submit" class="step-footer-continue-btn btn">
+                                    <span class="btn-content">Hoàn tất đơn hàng</span>
+                                </button></Link>
+
+                            </div>
+                        </div>
 
 
                     </div >
@@ -161,7 +238,7 @@ function CheckOuts() {
                 <div className='box-right col-md-6'>
                     <div className="main-header">
 
-                        dhdygjkuiki
+
                     </div>
                 </div>
             </div>
